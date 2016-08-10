62716ffa58cdfda66b734d057167fb34a1ac9b29
AustinMahan <amahan0424@gmail.com>
2016-08-10 11:29:08 -0600

    fixing heroku

--- main.js
+++ main.js
@@ -2,13 +2,14 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+var port = process.env.PORT || 2000;
 
 //points the sesrver to the index.html file
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 app.use('/',express.static(__dirname + '/'));
-serv.listen(2000);
+serv.listen(port);
 console.log("Server started.");
 
 
